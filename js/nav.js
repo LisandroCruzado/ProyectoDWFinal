cae434d76f320df350c1f61c68fc9665be9f30be
Lisandro Cruzado <seveen.cs@msn.com>
2023-02-02 01:49:15 -0300

    Mi primer commit

---
+++ js/nav.js
@@ -0,0 +1,9 @@
+// Nav bar //
+
+function openNav(){
+    document.getElementById("mobile-menu").style.width = "100%";
+}
+function closeNav(){
+    document.getElementById("mobile-menu").style.width = "0%";
+}
+
